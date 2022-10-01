16044c3338d3cc9dfb13c54aa656b72618299d6a
Crypt0head <zelas.money@yandex.ru>
2022-10-01 21:16:29 +0300

    Homework 1

---
+++ H1/T2.py
@@ -0,0 +1,5 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+x, y, z = input("Input three predicates x, y, z: ").split()
+
+print(not (x and y and z) == ( not x or not y or not z))
